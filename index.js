9033f5a03c88d0d498a353ec9e8057f453f80d58
Kamal Doharey <kamal@axebox.in>
2022-05-17 18:58:09 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function btn_add(){
+    const div1 = document.getElementById('text-result');
+    var current_value =  div1.innerText;
+    const current_value_in_num = Number(current_value);
+    // text
+    div1.textContent =  current_value_in_num + 1 ;
+}
+function btn_save(){
+    const div1 = document.getElementById('text-result');
+    var current_value =  div1.innerText;    
+    const saved_value = document.getElementById('text-result').innerText;
+    const saved_value_span = document.getElementById('saved_value');
+    saved_value_span.textContent = current_value;
+    div1.textContent = "0";
+}
